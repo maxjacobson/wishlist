88a6434f1741f618133b9dce59c95267789d04df
Max Jacobson <max@hardscrabble.net>
2014-11-30 17:09:46 -0500

    Display nice message when the list is unavailable

--- web.rb
+++ web.rb
@@ -5,9 +5,14 @@ require 'httparty'
 
 get '/' do
   @title = "Wish List #{Time.now.year}"
-  @list = HTTParty.get(ENV['WISHLIST_URL']).to_s
   unless ENV['WISHLIST_URL']
     raise Exception, "Must set ENV['WISHLIST_URL']"
+  end
+  response = HTTParty.get(ENV['WISHLIST_URL'])
+  @list = if response.code == 200
+    response.to_s
+  else
+    "Sorry, the list isn't available"
   end
   erb :wishlist
 end
