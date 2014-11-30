4e4f0ba9fe150dead930daa114493ad019125b54
Max Jacobson <max@hardscrabble.net>
2014-11-30 17:09:25 -0500

    Raise an exception when env variable is missing

--- web.rb
+++ web.rb
@@ -6,6 +6,9 @@ require 'httparty'
 get '/' do
   @title = "Wish List #{Time.now.year}"
   @list = HTTParty.get(ENV['WISHLIST_URL']).to_s
+  unless ENV['WISHLIST_URL']
+    raise Exception, "Must set ENV['WISHLIST_URL']"
+  end
   erb :wishlist
 end
 
